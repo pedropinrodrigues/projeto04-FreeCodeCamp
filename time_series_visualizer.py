fcf121ec424898da930f87054274c89ae0a70069
pedropinrodrigues <pedro.rodrigues@sou.inteli.edu.br>
2024-09-24 17:58:26 -0300

    finaliza projeto

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -13,38 +13,45 @@ df = df[(df['value'] < df['value'].quantile(0.975)) & (df['value'] > df['value']
 
 
 def draw_line_plot():
-    plt.figure(figsize=(13, 5))
+    fig = plt.figure(figsize=(13, 5))
     plt.plot(df.index, df['value'], label='line')
     plt.xlabel('Date')
     plt.ylabel('Page Views')
     plt.title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
-    fig = plt.show()
-
+    
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
     return fig
 
 def draw_bar_plot():
-    # Copy and modify data for monthly bar plot
     df_bar_plot = df.copy()
 
     df_bar_plot['year'] = df_bar_plot.index.year
     df_bar_plot['month'] = df_bar_plot.index.month
-
-    # Draw bar plot
-    anos = df_bar_plot['year'].unique()
     meses = df_bar_plot['month'].unique()
-    dados = {}
+    anos = df_bar_plot['year'].unique()
+    meses.sort()
+    meses_str = ['January', 'February', 'March', 'April', 'May', 'June', 
+                'July', 'August', 'September', 'October', 'November', 'December']
+
+
+    meses_dict = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 
+                7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
 
     for mes in meses:
+        df_bar_plot['month'] = df_bar_plot['month'].replace(mes, meses_dict[mes])
+
+    dados = {}
+
+    for mes in meses_str:
         for ano in anos:
             valor = df_bar_plot[(df_bar_plot['month'] == mes) & (df_bar_plot['year'] == ano)]['value'].sum()
-            
             
             if mes not in dados:
                 dados[mes] = [valor]  
             else:
                 dados[mes].append(valor) 
+
 
     x = np.arange(len(anos))  
     width = 0.05  
@@ -60,12 +67,10 @@ def draw_bar_plot():
     ax.set_xticks(x + width * (len(meses) - 1) / 2)  
     ax.set_xticklabels(anos)
 
-    ax.set_xlabel('Year')
+    ax.set_xlabel('Years')
     ax.set_ylabel('Average Page Views')
     ax.set_title('Sales Comparison by Year and Month')
     ax.legend(loc='upper left', ncols=4)
-
-    plt.show()
 
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
@@ -90,6 +95,7 @@ def draw_box_plot():
     axis[0].set_ylabel('Page Views', fontsize=18)  
     axis[0].tick_params(axis='both', which='major', labelsize=16)  
     axis[0].set_ylim(0, 200000)
+    axis[0].set_yticks(np.arange(0, 200001, 20000))
 
     # Second subplot - Boxplot for months
     sns.boxplot(x="month", y="value", data=df_box, ax=axis[1])
@@ -98,12 +104,10 @@ def draw_box_plot():
     axis[1].set_ylabel('Page Views', fontsize=18)  
     axis[1].tick_params(axis='both', which='major', labelsize=16)  
     axis[1].set_ylim(0, 200000)
+    axis[1].set_yticks(np.arange(0, 200001, 20000))
 
     # Adjust the layout
     plt.tight_layout()
-
-    # Show the plot
-    plt.show()
 
     # Save image and return fig (don't change this part)
     fig.savefig('box_plot.png')
